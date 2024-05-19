6b29afa6a994b338705f8866c57ae7c88c07ee92
Chig00 <chiago321@gmail.com>
2024-05-19 17:24:33 +0100

    Use the CppFlow example (output is still suppressed)

--- src/main/cpp/nnsmall/nnloadsmall.hpp
+++ src/main/cpp/nnsmall/nnloadsmall.hpp
@@ -4,13 +4,16 @@
 #include <vector>
 #include <cstdio>
 #include <cstdlib>
-#include <tensorflow/c/c_api.h>
+#include <cppflow/cppflow.h>
 
 namespace NeuralRun {
     namespace NNSmall {
         void run(const std::vector<std::string>& arguments) {
-            std::cout << TF_Version() << std::endl;
-            std::printf("%s\n", TF_Version());
+            // Create a tensor from a list, a = [1.0, 2.0, 3.0]
+            auto a = cppflow::tensor({1.0, 2.0, 3.0});
+            // Create a tensor of shape 3 filled with 1.0, b = [1.0, 1.0, 1.0]
+            auto b = cppflow::fill({3}, 1.0);
+            std::cout << a + b << std::endl;
         }
     }
 }
