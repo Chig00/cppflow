655013ddcc54c9ea624d813ae1a1b4a812f2c78c
Chig00 <chiago321@gmail.com>
2024-05-26 16:35:50 +0100

    Demonstrate the quirks of the CPPFlow tensor operations

--- src/main/cpp/nnsmall/nnloadsmall.hpp
+++ src/main/cpp/nnsmall/nnloadsmall.hpp
@@ -9,8 +9,18 @@
 namespace NeuralRun {
     namespace NNSmall {
         void run(const std::vector<std::string>& arguments) {
-            std::cout << cppflow::tensor(std::vector<int>({1, 2, 3, 4, 5, 6, 7}),
-                                         std::vector<int64_t>({3, 2})) << std::endl;
+            cppflow::tensor a(std::vector<int>({1, 2}), std::vector<int64_t>({1, 2}));
+            cppflow::tensor b(std::vector<int>({1, 2}), std::vector<int64_t>({2, 1}));
+            cppflow::tensor c(a + b);
+            cppflow::tensor d(a * b);
+            cppflow::tensor e(b * a);
+            cppflow::tensor f(cppflow::mat_mul(a, b));
+            std::cout << a << std::endl
+                      << b << std::endl
+                      << c << std::endl
+                      << d << std::endl
+                      << e << std::endl
+                      << f << std::endl;
         }
     }
 }
