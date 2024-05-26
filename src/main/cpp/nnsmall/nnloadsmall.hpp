3112d14f437db075f19ab298df5b66fda55f82ba
Chig00 <chiago321@gmail.com>
2024-05-26 19:53:39 +0100

    Support the foundations of the result renderer

--- src/main/cpp/nnsmall/nnloadsmall.hpp
+++ src/main/cpp/nnsmall/nnloadsmall.hpp
@@ -2,13 +2,48 @@
 
 #include "../util.hpp"
 
-constexpr std::string FILE_NAME("models/nnsmall");
-constexpr float START = 0;
-constexpr float STOP = 1;
-constexpr float INCREMENT = 0.333;
-
 namespace NeuralRun {
     namespace NNSmall {
+        constexpr std::string FILE_NAME("models/nnsmall");
+        
+        constexpr float START = 0;
+        constexpr float STOP = 1;
+        constexpr float INCREMENT = 0.333;
+        
+        constexpr int WINDOW_WIDTH = 400;
+        constexpr int WINDOW_HEIGHT = 400;
+        constexpr int POSITIVE_SET_WIDTH = WINDOW_WIDTH / 2;
+        constexpr int POSITIVE_SET_HEIGHT = WINDOW_HEIGHT / 2;
+        constexpr int POSITIVE_SET_X = WINDOW_WIDTH - POSITIVE_SET_WIDTH;
+        constexpr int POSITIVE_SET_Y = 0;
+        
+        void present() {
+            System::initialise();
+            Display display(WINDOW_WIDTH, WINDOW_HEIGHT);
+            Rectangle positive_set_rectangle(POSITIVE_SET_X,
+                                             POSITIVE_SET_Y,
+                                             POSITIVE_SET_WIDTH,
+                                             POSITIVE_SET_HEIGHT);
+            display.fill(positive_set_rectangle, Sprite::WHITE);
+            display.update();
+            Event event;
+            bool should_present = true;
+            
+            while (should_present) {
+                while (should_present && event.poll()) {
+                    switch (event.type()) {
+                        case Event::TERMINATE:
+                        case Event::LEFT_CLICK:
+                        case Event::KEY_PRESS:
+                            should_present = false;
+                            break;
+                    }
+                }
+            }
+            
+            System::terminate();
+        }
+        
         void run(const std::vector<std::string>& arguments) {
             cppflow::model model(FILE_NAME);
             std::cout << std::endl;
@@ -28,6 +63,8 @@ namespace NeuralRun {
                               << output_class << std::endl;
                 }
             }
+            
+            present();
         }
     }
 }
